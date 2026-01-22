21d9c08e09ec1651ec5f24a5a6786821005d0ddd
ChandanKT-git <chandukt29092004@gmail.com>
2026-01-23 00:18:57 +0530

    chore: initial project setup and database connection

---
+++ server/index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
+import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
+
+import path from "path";
+
+dotenv.config({});
+
+// call database connection here
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// default middleware
+app.use(express.json({
+    verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+    }
+}));
+app.use(cookieParser());
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
+}));
+
+// apis
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
+
+const __dirname = path.resolve();
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+})
+
+
